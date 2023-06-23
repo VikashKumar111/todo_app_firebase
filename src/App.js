bedca0ecdaa215d70c2473f254c17381b1adf792
Vikash Kumar <vikasthakur9310@gmail.com>
2023-06-23 19:27:44 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for a walk','Take the rubbish out', 'Vikas wants to livestrem today']);
+  const [input, setInput] = useState('');
+  console.log(input);
+  const addTodo = (event) => {
+    console.log('i m coding');
+    setTodos([...todos, input]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello clever programmer</h1>
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
+      
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+        
+      </ul>
     </div>
   );
 }
